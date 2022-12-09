502510501a3ea23a8888a0db6fbc76addd47c256
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-09 11:31:04 +0100

    Get header information

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,12 +20,16 @@ public class Main {
             BufferedReader inputStream = new BufferedReader(
                     new InputStreamReader(urlConnection.getInputStream()));
 
-            String line = "";
-            while (line != null) {
-                line = inputStream.readLine();
-                System.out.println(line);
+            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+            for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+                String key = entry.getKey();
+                List<String> value = entry.getValue();
+                System.out.println("***** key: " + key);
+
+                for (String string : value) {
+                    System.out.println("value: " + value);
+                }
             }
-            inputStream.close();
         } catch (MalformedURLException e) {
             System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {
