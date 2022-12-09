956e51b96cbc83bd4c7eedfc456ede90d9660e31
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-09 11:01:22 +0100

    Reading HTML file #2

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -3,15 +3,20 @@ package com.tg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
 
             BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(url.openStream()));
+                    new InputStreamReader(urlConnection.getInputStream()));
 
             String line = "";
             while (line != null) {
@@ -19,6 +24,8 @@ public class Main {
                 System.out.println(line);
             }
             inputStream.close();
+        } catch (MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
