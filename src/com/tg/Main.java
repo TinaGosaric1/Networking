6c1626989f4fa67b04e70cc5047fb66300c123c4
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-09 10:41:43 +0100

    Reading HTML file

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -1,38 +1,26 @@
 package com.tg;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            URI baseUri = new URI("http://username:password@mynewserver.com:5000");
-            URI uri1 = new URI("/catalogue/phones?os=android#samsung");
-            URI uri2 = new URI("/catalogue/tvs?manufacturer=samsung");
-            URI uri3 = new URI("/stores/locations?zip=12345");
+            URL url = new URL("http://example.org");
 
-            URI resolvedUri1 = baseUri.resolve(uri1);
-            URI resolvedUri2 = baseUri.resolve(uri2);
-            URI resolvedUri3 = baseUri.resolve(uri3);
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
 
-            URL url1 = resolvedUri1.toURL();
-            System.out.println("URL 1: " + url1);
-
-            URL url2 = resolvedUri2.toURL();
-            System.out.println("URL 2: " + url2);
-
-            URL url3 = resolvedUri3.toURL();
-            System.out.println("URL 3: " + url3);
-
-        } catch (URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
-
-        } catch (MalformedURLException e) {
-            System.out.println("URL Malformed: " + e.getMessage());
+            String line = "";
+            while (line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
-
-
     }
 }
