d842f19e4f012893ab330b80b415ed7f4c24aa8a
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-09 12:11:44 +0100

    HttpURLConnection + return response code

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -3,33 +3,39 @@ package com.tg;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
 
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream()));
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
-            for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
-                String key = entry.getKey();
-                List<String> value = entry.getValue();
-                System.out.println("***** key: " + key);
-
-                for (String string : value) {
-                    System.out.println("value: " + value);
-                }
+            if (responseCode != 200) {
+                System.out.println("Error reading web page");
+                System.out.println(connection.getResponseMessage());
+                return;
             }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+
+            while ((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+
         } catch (MalformedURLException e) {
             System.out.println("Malformed URL: " + e.getMessage());
         } catch (IOException e) {
